ad3acf6a911d1457f89da5d7975d21867888d36d
Hasbi-DOM <hasbiishlahy19@gmail.com>
2026-01-18 10:20:59 +0700

    :art: Add middleware

---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+	const token = request.cookies.get('access_token')?.value
+	const { pathname } = request.nextUrl
+
+	if (!token && pathname.startsWith('/main')) {
+		return NextResponse.redirect(new URL('/', request.url))
+	}
+
+	if (token && pathname === '/') {
+		return NextResponse.redirect(new URL('/main/dashboard', request.url))
+	}
+
+	return NextResponse.next()
+}
+
+export const config = {
+	matcher: ['/main/:path*', '/']
+}
