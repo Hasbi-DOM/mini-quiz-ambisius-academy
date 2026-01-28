c1d9b4fb3dc24a5dc0269dff02b93ad0585660fd
Hasbi-DOM <hasbiishlahy19@gmail.com>
2026-01-28 11:05:42 +0700

    :lipstick: add layout main

--- src/app/main/layout.js
+++ src/app/main/layout.js
@@ -1,12 +1,30 @@
+import { cookies } from "next/headers"
 import { Header } from "../_components/layout/header"
+import ProfileProvider from "../context/profile-context"
 
-export default function MainLayout({ children }) {
+async function getProfile() {
+	const token = (await cookies()).get('access_token')?.value
+
+	const res = await fetch(
+		`${process.env.BACKEND_API_URL}/auth/profile`,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		}
+	)
+
+	if (!res.ok) return null
+	return res.json()
+}
+export default async function MainLayout({ children }) {
+	const profile = await getProfile()
 	return (
-	<>
+	<ProfileProvider value={profile}>
 		<Header />
 		<main className="px-8 py-24">
 			{children}
 		</main>
-	</>
+	</ProfileProvider>
 	)
 }
---
+++ src/app/main/quiz/layout.js
@@ -0,0 +1,7 @@
+export default async function QuizLayout({ children }) {
+	return (
+		<main>
+			{children}
+		</main>
+	)
+}
